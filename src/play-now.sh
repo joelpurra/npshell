ba21172c6c2402b8b4c8682f2d0e2dbc3fd159e9
Joel Purra <code+github@joelpurra.com>
2014-10-09 17:21:22 +0200

    Update play now script

--- src/play-now.sh
+++ src/play-now.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
+source "${BASH_SOURCE%/*}/play-shared-functions.sh"
 source "${BASH_SOURCE%/*}/play-shared-functionality.sh"
+source "${BASH_SOURCE%/*}/play-shared-functionality-mutexed.sh"
 
 highlight "$(getNextSound)"
