3b67dc4ee9c6ee209aa90b618a6ca77121428217
Joel Purra <code+github@joelpurra.com>
2014-10-12 10:51:27 +0200

    Check input when adding sounds

--- src/play-add.sh
+++ src/play-add.sh
@@ -5,7 +5,16 @@ source "${BASH_SOURCE%/*}/play-shared-functions.sh"
 source "${BASH_SOURCE%/*}/play-shared-functionality.sh"
 source "${BASH_SOURCE%/*}/play-shared-functionality-mutexed.sh"
 
-sharedNumsounds="${1:-$sharedNumsounds}"
-sharedOrder="${2:-$sharedOrder}"
+if isValidStrictlyPositiveNumber "$1";
+then
+	sharedNumsounds="${1:-$sharedNumsounds}"
+	shift
+fi
+
+if isValidPlayOrder "$1";
+then
+	sharedOrder="${1:-$sharedOrder}"
+	shift
+fi
 
 getSounds | playOrder | limit | absoluteSoundPaths >> "$sharedQueueFile"
--- src/play-shared-functions.sh
+++ src/play-shared-functions.sh
@@ -74,6 +74,38 @@ shuffle() {
 	gshuf -z
 }
 
+isValidStrictlyPositiveNumber() {
+	declare -r str="$1"
+
+	if [[ "$str" =~ ^[[:digit:]]+$ ]];
+	then
+		# Check if $1 has been interpreted to something else than it was before.
+		# That is the strings "", "a", "abcdef" are all interpreted as zero.
+		# Only afterwards can $n assumed to be zero, not "zero or any value that was not a number."
+		# (Oh and the regex probably took care of it already, but wth.)
+		declare -i -r n="$str"
+
+		if [[ "$str" == "$n" ]] && (( n >= 0 ));
+		then
+			return 0
+		else
+			return 1
+		fi
+	else
+		return 1
+	fi
+}
+
+isValidPlayOrder() {
+	local order="$1"
+
+	if [[ "$order" == "random" || "$order" == "in-order" ]]; then
+		return 0
+	else
+		return 1
+	fi
+}
+
 playOrder() {
 	if [[ "$sharedOrder" == "random" ]]; then
 		shuffle
