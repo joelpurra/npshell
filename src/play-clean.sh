fa1e2a1798cbaff1cb3ed7d0c2fc4da07133341b
Joel Purra <code+github@joelpurra.com>
2014-11-24 07:37:41 +0100

    Fix 'play clean'

--- src/play-clean.sh
+++ src/play-clean.sh
@@ -5,8 +5,23 @@ source "${BASH_SOURCE%/*}/shared/functions.sh"
 source "${BASH_SOURCE%/*}/shared/functionality.sh"
 source "${BASH_SOURCE%/*}/shared/mutexed.sh"
 
+tmpQueueFile="$sharedQueueFile.tmp~"
+
 skipNonexistantFiles() {
-	[[ -s "$1" ]] && { echo -nE "$1"; echo -ne "\0"; }
+	sound="$1"
+
+	debug "clean: checking '${sound}'."
+
+
+	if [[ -s "${sound}" ]];
+	then
+		debug "clean: kept existing sound '${sound}' in queue."
+		echo -nE "${sound}"
+		echo -ne "\0"
+	else
+		debug "clean: removed sound '${sound}' from queue."
+	fi
 }
 
-{ cat "$sharedQueueFile" | nullDelimitedForEachWithEOF skipNonexistantFiles > "$sharedQueueFile.tmp~"; } && mv "$sharedQueueFile.tmp~" "$sharedQueueFile"
+cat "$sharedQueueFile" | nullDelimitedForEachWithEOF skipNonexistantFiles > "$tmpQueueFile"
+mv -f "$tmpQueueFile" "$sharedQueueFile"
