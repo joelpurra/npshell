c026c06c6cd8a44e2378a709faa2d39988207933
Joel Purra <code+github@joelpurra.com>
2014-12-01 09:23:34 +0100

    Also stop playback after clearing queue

--- src/np-clear.sh
+++ src/np-clear.sh
@@ -6,3 +6,6 @@ source "${BASH_SOURCE%/*}/shared/functionality.sh"
 source "${BASH_SOURCE%/*}/shared/mutexed.sh"
 
 echo -n '' > "$configQueueFile"
+
+# TODO: define a "next" function, as well as for other common user commands.
+killExternalPlayerIfRunning
