94250cd07ccc957ee1df24408b48cc63b01e83a4
Joel Purra <code+github@joelpurra.com>
2015-06-27 19:36:12 +0200

    More info in 'np doctor'

--- src/np-doctor.sh
+++ src/np-doctor.sh
@@ -5,7 +5,7 @@ source "${BASH_SOURCE%/*}/shared/functions.sh"
 source "${BASH_SOURCE%/*}/shared/functionality.sh"
 source "${BASH_SOURCE%/*}/shared/mutexed.sh"
 
-displayMessage "Configuration: $configConfigFolder"
+displayMessage "Configuration: '$configConfigFolder'"
 
 
 displayMessage "External player: '$externalPlayerExec'"
@@ -13,9 +13,10 @@ displayMessage "fswatch: '$fswatchExec'"
 displayMessage "Shuffler: '$externalShuffleExec'"
 displayMessage "Line reverser: '$reverseLineOrderExec'"
 displayMessage "Notifications: '$notifyExec'"
+displayMessage "Sound details: '$soundDetailsExec'"
 
 
-displayMessage "Mode: $(cat "$configModeFile")"
+displayMessage "Mode: '$(cat "$configModeFile")'"
 
 
 displayMessage -n "Daemon: "
@@ -45,4 +46,20 @@ then
 	highlight "$(cat "$configPlayingFile")" || displayMessage
 else
 	displayMessage "stopped"
+fi
+
+displayMessage -n "Sounds in queue: "
+if [[ -e "$configQueueFile" ]];
+then
+	cat "$configQueueFile" | nullAsNewline wc -l | keepDigitsOnly
+else
+	displayMessage "(queue file missing, expected: '${configQueueFile}')"
+fi
+
+displayMessage -n "Sounds in history: "
+if [[ -e "$configHistoryFile" ]];
+then
+	cat "$configHistoryFile" | nullAsNewline wc -l | keepDigitsOnly
+else
+	displayMessage "(history file missing, expected: '${configHistoryFile}')"
 fi
