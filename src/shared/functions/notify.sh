46f0240caf41141014a2f0d8f155142ff9aa7135
Joel Purra <mig@joelpurra.se>
2017-12-12 19:42:48 +0100

    Assume terminal-notifier v2.0.0 is used, and remove alerter flags
    - Versions before terminal-notifier v2.0.0 had bugs related to "alerter" mode notifications.
    - Workarounds were in place in `npshell`.
    - Removed workarounds, assuming terminal-notifier v2.0.0 or higher is being used.
    
    See
    
    - https://github.com/julienXX/terminal-notifier/issues/223
    - https://github.com/julienXX/terminal-notifier/releases/tag/2.0.0

--- src/shared/functions/notify.sh
+++ src/shared/functions/notify.sh
@@ -45,30 +45,16 @@ notify() {
 			(
 				# NOTE: forcefully close any previous notification, in case it wasn't already gone.
 				terminal-notifier \
-					-remove "np" \
-					-json >/dev/null
+					-remove "np" >/dev/null
 
-				# TODO: use the JSON output if the user clicks close/show/custom action?
-				# The closeLabel and alert actions are only shown if the user allows it to.
-				# - Open System Preferences > Notification.
-				# - Select "terminal-notifier" (or perhaps "np") in the sidebar.
-				# - Select the "Alerts" alert style.
-				# https://github.com/julienXX/terminal-notifier/raw/master/assets/System_prefs.png
-				#
-				# NOTE: Setting timeout due to bug in terminal-notifier.
-				# https://github.com/julienXX/terminal-notifier/issues/223
-				#
-				# NOTE: escaping the first character of title/subtitle/message, despite the bug being fixed.
-				# https://github.com/julienXX/terminal-notifier/issues/11
+				# NOTE: need to escaping the first character of title/subtitle/message; at least special characters such as square brackets '['.
 				terminal-notifier \
 					-group "np" \
 					-title "\\${title}" \
 					-open "https://github.com/joelpurra/npshell" \
 					-subtitle "\\${subtitle}" \
-					-message "\\${message}" \
-					-timeout "$configNotificationTimeout" \
-					-json >/dev/null
-			)  &
+					-message "\\${message}" >/dev/null
+			) &
 			;;
 		'growlnotify')
 			# http://growl.info/downloads
